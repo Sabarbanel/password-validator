4f1ebdad1b4e68e84c939f49f515eb1ff335defa
Sarah Abarbanel <scabarbanel@gmail.com>
2018-05-29 22:38:30 -0300

    Adding the activity reactions to screen events

--- app/src/main/java/com/example/sarah/passwordval/ValidateActivity.java
+++ app/src/main/java/com/example/sarah/passwordval/ValidateActivity.java
@@ -1,7 +1,12 @@
 package com.example.sarah.passwordval;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ValidateActivity extends AppCompatActivity {
 
@@ -9,5 +14,24 @@ public class ValidateActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_validate);
+
+        Button validateButton = findViewById(R.id.button);
+        validateButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                TextView isValidated = findViewById(R.id.textView);
+                EditText password = findViewById(R.id.editText);
+                String passwordMessage;
+                if (Validator.validate(password.getText().toString()) < 5) {
+                    passwordMessage = "This password is too weak";
+                } else {
+                    passwordMessage = "This password is sufficiently strong";
+                }
+
+                isValidated.setText(passwordMessage);
+            }
+        });
+
+
     }
 }
