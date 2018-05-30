ba73cece75c4d2870d460e8da7c9ea88adb70f58
Sarah Abarbanel <scabarbanel@gmail.com>
2018-05-29 22:31:49 -0300

    writing espresso tests

--- app/src/androidTest/java/com/example/sarah/passwordval/EspressoTests.java
+++ app/src/androidTest/java/com/example/sarah/passwordval/EspressoTests.java
@@ -2,9 +2,15 @@ package com.example.sarah.passwordval;
 
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 
 @RunWith(AndroidJUnit4.class)
@@ -12,17 +18,58 @@ import org.junit.runner.RunWith;
 public class EspressoTests {
 
     @Test
-    public void textDisplaysOnButtonClick()
-    {
-        //assign textfield
-        //assign button
-        //assign textdisplay
+    public void lowerCaseIssue() {
+        onView(withId(R.id.editText)).perform(typeText("rooo1234oooof"));
+        onView(withId(R.id.button)).perform(click());
+        onView(withId(R.id.textView)).check(matches(withText("This password is too weak")));
+    }
+    @Test
+    public void upperCaseIssue() {
+        onView(withId(R.id.editText)).perform(typeText("ROOO1234OOOOF"));
+        onView(withId(R.id.button)).perform(click());
+        onView(withId(R.id.textView)).check(matches(withText("This password is too weak")));
+    }
+    @Test
+    public void namedPasswordIssue() {
+        onView(withId(R.id.editText)).perform(typeText("paSsword"));
+        onView(withId(R.id.button)).perform(click());
+        onView(withId(R.id.textView)).check(matches(withText("This password is too weak")));
+    }
+    @Test
+    public void failsRegex() {
+        onView(withId(R.id.editText)).perform(typeText("paSsword1234"));
+        onView(withId(R.id.button)).perform(click());
+        onView(withId(R.id.textView)).check(matches(withText("This password is too weak")));
+    }
+    @Test
+    public void failsLetterReq() {
+        onView(withId(R.id.editText)).perform(typeText("12345678"));
+        onView(withId(R.id.button)).perform(click());
+        onView(withId(R.id.textView)).check(matches(withText("This password is too weak")));
+    }
+    @Test
+    public void failsNumberReq() {
+        onView(withId(R.id.editText)).perform(typeText("FloooooooofyCat"));
+        onView(withId(R.id.button)).perform(click());
+        onView(withId(R.id.textView)).check(matches(withText("This password is too weak")));
+    }
+    @Test
+    public void verifyPassing() {
+        onView(withId(R.id.editText)).perform(typeText("13%CAtsP\\"));
+        onView(withId(R.id.button)).perform(click());
+        onView(withId(R.id.textView)).check(matches(withText("This password is sufficiently strong")));
+    }
+    @Test
+    public void verifyClickingWorksWhenOverpressed() {
+        onView(withId(R.id.editText)).perform(typeText("R5(9grite34"));
+        onView(withId(R.id.button)).perform(click());
+        onView(withId(R.id.button)).perform(click());
+        onView(withId(R.id.button)).perform(click());
+        onView(withId(R.id.button)).perform(click());
+        onView(withId(R.id.textView)).check(matches(withText("This password is sufficiently strong")));
+    }
 
-        //focus on textfield
-        //enter text
-        //minimize keyboard
-        //click on button
-        // verify the text in the display is the message it's supposed to be.
+
 
     }
 
