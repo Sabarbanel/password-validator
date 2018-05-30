95c1e086ed6dfa791e794f1a48814dffd34068f9
Sarah Abarbanel <scabarbanel@gmail.com>
2018-05-29 21:51:52 -0300

    setting up logic for the UI test

--- app/src/androidTest/java/com/example/sarah/passwordval/EspressoTests.java
+++ app/src/androidTest/java/com/example/sarah/passwordval/EspressoTests.java
@@ -3,13 +3,27 @@ package com.example.sarah.passwordval;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
-public class EspressoTests  {
+public class EspressoTests {
 
+    @Test
+    public void textDisplaysOnButtonClick()
+    {
+        //assign textfield
+        //assign button
+        //assign textdisplay
+
+        //focus on textfield
+        //enter text
+        //minimize keyboard
+        //click on button
+        // verify the text in the display is the message it's supposed to be.
 
     }
+    
 }
