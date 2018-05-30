320edea72ec095b6d18ba67f317769f93c887d86
Sarah Abarbanel <scabarbanel@gmail.com>
2018-05-29 21:16:11 -0300

    Setting the groundwork for espresso tests

---
+++ app/src/androidTest/java/com/example/sarah/passwordval/EspressoTests.java
@@ -0,0 +1,15 @@
+package com.example.sarah.passwordval;
+
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.runner.RunWith;
+
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class EspressoTests  {
+
+
+    }
+}
